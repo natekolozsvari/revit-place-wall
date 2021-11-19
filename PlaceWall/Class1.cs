706155e929d1b410ea79a8d2e3ca5017c332fc54
natekolozsvari <nate.kolozsvari@gmail.com>
2021-11-19 08:24:33 +0100

    Create addin to place wall between two previously defined coordinates

--- PlaceWall/Class1.cs
+++ PlaceWall/Class1.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 
 namespace PlaceWall
 {
-    public class Class1
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
     {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            XYZ start = new XYZ(0, 0, 0);
+            XYZ end = new XYZ(20, 0, 0);
+
+            Line line = Line.CreateBound(start, end);
+
+            FilteredElementCollector levels = new FilteredElementCollector(doc).OfClass(typeof(Level));
+            Level firstLevel = levels.FirstElement() as Level;
+            
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("PlaceWall");
+            Wall.Create(doc, line, firstLevel.Id, true);
+            transaction.Commit();
+
+            return Result.Succeeded;
+            
+        }
     }
 }
